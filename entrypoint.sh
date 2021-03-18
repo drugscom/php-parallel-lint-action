0e4f0349b5778f376d87445d413fb575b0d661f8
Andre Sencioles <asenci@gmail.com>
2021-03-19 12:24:45 +1300

    Enable --short by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ set -o errexit
 REPORT_FILE=$(mktemp)
 
 /usr/local/bin/parallel-lint \
-  --json \
   -e php,html \
+  --short \
+  --json \
   "${@}" > "${REPORT_FILE}" || PARALLEL_LINT_STATUS=${?}
 
 test -f "${REPORT_FILE}" && (jq --raw-output '.results.errors[] | "::error " + "file=" + .file + ",line=" + (.line|tostring) + "::" + .normalizeMessage' "${REPORT_FILE}" || cat "${REPORT_FILE}")
